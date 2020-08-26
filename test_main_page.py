d12a75cc8621117671e27ac86c77879fdee6cf26
David <david.zhenetl@gmail.com>
2020-08-26 11:56:33 +0300

    Login Page Object

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,12 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_do_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    # page = MainPage(browser, link)
+    # page.open()
+    # page.should_be_login_link()
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_page()
