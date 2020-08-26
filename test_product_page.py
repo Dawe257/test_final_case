bb349679ac58d603f9794f5891d25c02475664e8
David <david.zhenetl@gmail.com>
2020-08-26 17:53:32 +0300

    test_product_page.py update

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,14 @@
+import pytest
 import time
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('link', [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num}" for num in range(10)])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.add_to_cart_message_should_be_correct()
     page.cart_total_is_correct()
+
